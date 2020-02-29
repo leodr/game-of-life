ecc01a2c2ba67a2f083d636156bd8108e7160887
Leo Driesch <leo.driesch@gmail.com>
2020-02-29 18:34:42 +0100

    :sparkles: Add function to calculate next game state

---
+++ src/getNextGameState.ts
@@ -0,0 +1,53 @@
+const getAliveNeighborCount = (
+    game: boolean[][],
+    x: number,
+    y: number
+): number => {
+    const neighborList = [];
+
+    neighborList.push(game[x - 1]?.[y - 1]);
+    neighborList.push(game[x - 1]?.[y]);
+    neighborList.push(game[x - 1]?.[y + 1]);
+    neighborList.push(game[x]?.[y - 1]);
+    neighborList.push(game[x]?.[y + 1]);
+    neighborList.push(game[x + 1]?.[y - 1]);
+    neighborList.push(game[x + 1]?.[y]);
+    neighborList.push(game[x + 1]?.[y + 1]);
+
+    return neighborList.reduce((acc, e) => (e ? acc + 1 : acc), 0);
+};
+
+const getNextGameState = (currentState: boolean[][]): boolean[][] => {
+    const newState: boolean[][] = [];
+
+    for (let x = 0; x < currentState.length; x++) {
+        for (let y = 0; y < currentState[x].length; y++) {
+            if (newState[x] !== undefined) {
+                newState[x].push(false);
+            } else {
+                newState[x] = [false];
+            }
+        }
+    }
+
+    for (let x = 0; x < currentState.length; x++) {
+        for (let y = 0; y < currentState[x].length; y++) {
+            const currentCell = currentState[x][y];
+            const aliveNeighbors = getAliveNeighborCount(currentState, x, y);
+
+            if (currentCell) {
+                if (aliveNeighbors >= 2 && aliveNeighbors <= 3) {
+                    newState[x][y] = true;
+                }
+            } else {
+                if (aliveNeighbors === 3) {
+                    newState[x][y] = true;
+                }
+            }
+        }
+    }
+
+    return newState;
+};
+
+export default getNextGameState;
