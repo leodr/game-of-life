198e4b066717dd284390ed6c4bc2634339c60175
Leo Driesch <leo.driesch@gmail.com>
2020-02-29 18:35:10 +0100

    :sparkles: Start developing control panel

---
+++ src/ControlPanel.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface props {
+    pause
+}
+
+const ControlPanel:  = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ControlPanel
---
+++ src/StateProvider.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const StateContext = React.createContext(undefined);
+
+interface props {
+    children?: any;
+}
+
+const initialState = {
+    running: true
+};
+
+const StateProvider: React.FC<props> = ({ children }) => {
+    const value = useState(initialState);
+
+    return (
+        <StateContext.Provider value={value}>{children}</StateContext.Provider>
+    );
+};
+
+export default StateProvider;
