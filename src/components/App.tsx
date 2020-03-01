664f25aac74b391877997a6e854812492f9a8def
Leo Driesch <leo.driesch@gmail.com>
2020-03-01 12:16:12 +0100

    :truck: Extract game component from app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
-import getNextGameState from '../logic/getNextGameState';
-import initializeGame from '../logic/initializeGame';
-import Tile from './App/Tile';
-
-const x = 50;
-const y = 50;
+import Game from './App/Game';
 
 const GlobalStyle = createGlobalStyle`
+    html { font-family: 'Inter', sans-serif; }
+
+    @supports (font-variation-settings: normal) {
+        html { font-family: 'Inter var', sans-serif; }
+    }
+
     body {
         margin: 0;
         font-family: Inter;
         background-color: #212121;
     }
 `;
-
-interface GameContainerProps {
-    x: number;
-    y: number;
-}
 
 const Root = styled.div`
     display: flex;
@@ -28,44 +24,11 @@ const Root = styled.div`
     width: 100vw;
 `;
 
-const GameConainer = styled.div`
-    display: grid;
-
-    @media (orientation: portrait) {
-        width: 80vw;
-        height: 80vw;
-    }
-
-    @media (orientation: landscape) {
-        width: 80vh;
-        height: 80vh;
-    }
-
-    grid-gap: 4px;
-    grid-template-columns: repeat(
-        ${(props: GameContainerProps) => props.x},
-        1fr
-    );
-    grid-auto-rows: 1fr;
-`;
-
 function App() {
-    const [game, setGame] = useState<boolean[][]>(initializeGame(x, y));
-
-    useEffect(() => {
-        const x = setInterval(() => {
-            setGame(s => getNextGameState(s));
-        }, 100);
-
-        return () => clearInterval(x);
-    });
-
     return (
         <>
             <Root>
-                <GameConainer x={x} y={y}>
-                    {game.map(e => e.map(e => <Tile alive={e} />))}
-                </GameConainer>
+                <Game />
             </Root>
             <GlobalStyle />
         </>
---
+++ src/components/App/Game.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import initializeGame from '../../logic/initializeGame';
+import Tile from './Game/Tile';
+
+const x = 50;
+const y = 50;
+
+interface GameContainerProps {
+    x: number;
+    y: number;
+}
+const GameConainer = styled.div`
+    display: grid;
+
+    @media (orientation: portrait) {
+        width: 80vw;
+        height: 80vw;
+    }
+
+    @media (orientation: landscape) {
+        width: 80vh;
+        height: 80vh;
+    }
+
+    grid-gap: 4px;
+    grid-template-columns: repeat(
+        ${(props: GameContainerProps) => props.x},
+        1fr
+    );
+    grid-auto-rows: 1fr;
+`;
+
+const Game = () => {
+    const [game, setGame] = useState<boolean[][]>(initializeGame(x, y));
+
+    useEffect(() => {
+        const x = setInterval(() => {
+            setGame(s => getNextGameState(s));
+        }, 100);
+
+        return () => clearInterval(x);
+    });
+
+    return (
+        <GameConainer x={x} y={y}>
+            {game.map(e => e.map(e => <Tile alive={e} />))}
+        </GameConainer>
+    );
+};
+
+export default Game;
